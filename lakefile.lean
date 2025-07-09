6e03e9b8ea9f98117255c6b436675c9b4f4cb6db
Nada Amin <namin@seas.harvard.edu>
2025-07-09 08:13:57 -0400

    update to v4.22.0-rc3

--- lakefile.lean
+++ lakefile.lean
@@ -13,4 +13,4 @@ lean_lib «MiniF2F» {
   -- add library configuration options here
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.20.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "360e0f4947562f2fc37350be3a3cc7db53247843"
